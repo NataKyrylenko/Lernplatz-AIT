94f3a02c161b7f35e4f9b542b5f9e4ea94be37db
NataKyrylenko <natalik10107919@gmail.com>
2023-11-10 21:03:35 +0100

    new commit

---
+++ src/HomeWorks/HomeWork2.java
@@ -0,0 +1,14 @@
+package HomeWorks;
+public class HomeWork2 {
+    public static void main(String[] args) {
+        String myName = "Nata";
+        int age = 36;
+        String country = "Ukraine";
+        System.out.println("My name " + myName);
+        System.out.println("I am " + age + " yahre old");
+        System.out.println("i am from " + country);
+
+
+
+    }
+}
---
+++ src/HomeWorks/HomeWork3.java
@@ -0,0 +1,16 @@
+package HomeWorks;
+public class HomeWork3 {
+    public static void main(String[] args) {
+
+        // Задача 1
+        double number1 = 25;
+        int number2 = 3;
+        System.out.println(number1/number2); // выводит результат деления
+        System.out.println(number1%number2); // выводит остаток от деления
+        
+        // Задача 2
+        int number3 = 8;
+        System.out.println(number3*number3); //   значение переменной в квадрате
+    }
+    
+}
