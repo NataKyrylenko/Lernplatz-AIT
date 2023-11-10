be73e3e57b1f139e5165e49fa8ffc8ad58f5ffc4
NataKyrylenko <natalik10107919@gmail.com>
2023-11-10 21:57:46 +0100

    init commit

--- src/HomeWorks/HomeWork4.java
+++ src/HomeWorks/HomeWork4.java
@@ -1,34 +1,34 @@
-// package HomeWorks;
+package HomeWorks;
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class HomeWork4 {
-//     public static void main(String[] args) {
+public class HomeWork4 {
+    public static void main(String[] args) {
 
-//         //Task1
-//         Scanner myScanner = new Scanner(System.in);
-//         String firstName = myScanner.nextLine();
-//         String lastName = myScanner.nextLine();
-//         int age = myScanner.nextInt();
-//         System.out.println("First name: " + firstName + "\nLast name: " + lastName + "\nAge: " + age);
+        //Task1
+        Scanner myScanner = new Scanner(System.in);
+        String firstName = myScanner.nextLine();
+        String lastName = myScanner.nextLine();
+        int age = myScanner.nextInt();
+        System.out.println("First name: " + firstName + "\nLast name: " + lastName + "\nAge: " + age);
 
-//         //Task2
-//         System.out.println("Enter a value char");
-//         Scanner newScanner = new Scanner(System.in);
-//         int myChar = newScanner.nextInt();
-//         System.out.println("Char value is: " + (char)myChar);
+        //Task2
+        System.out.println("Enter a value char");
+        Scanner newScanner = new Scanner(System.in);
+        int myChar = newScanner.nextInt();
+        System.out.println("Char value is: " + (char)myChar);
 
-//         //Task3
-//         System.out.println("Enter a value variable 1");
-//         Scanner newVariable = new Scanner(System.in);
-//         int num1 = newVariable.nextInt();
-//         System.out.println("Enter a value variable 2");
-//         int num2 = newVariable.nextInt();
+        //Task3
+        System.out.println("Enter a value variable 1");
+        Scanner newVariable = new Scanner(System.in);
+        int num1 = newVariable.nextInt();
+        System.out.println("Enter a value variable 2");
+        int num2 = newVariable.nextInt();
 
-//         num1 = num1 + num2;
-//         num2 = num1 - num2;
-//         num1 = num1 - num2;
-//         System.out.println("vaiables 1 = " + num1 + "\nvariables 2 = " + num2 );
-//     }
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.println("vaiables 1 = " + num1 + "\nvariables 2 = " + num2 );
+    }
 
-// }
+}
