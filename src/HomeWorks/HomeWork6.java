7157a2001a48a509d8a9bf9cf58a69b5bd8e2e2f
NataKyrylenko <natalik10107919@gmail.com>
2023-11-15 07:30:42 +0100

    add HomeWork6

---
+++ src/HomeWorks/HomeWork6.java
@@ -0,0 +1,28 @@
+package HomeWorks;
+
+public class HomeWork6 {
+    public static void main(String[] args) {
+       // Task1
+        // int a = 4, b = 3;
+        // if (a!=b) {
+        //     System.out.println(a+b);
+        //  }
+        // else if(a==b) {
+        //     System.out.println((a+b)*2);
+        // }
+
+        // Task2 
+        int c = 1, d = 6, e = 6;
+
+         if (c == d && d == e) {
+            System.out.println("All numbers are equal");
+         }
+         else if (c!=d && d!=e && c!=e ) {
+            System.out.println("All numbers are different");
+         }
+         else {
+            System.out.println("Some numbers are equal");
+         }
+
+    }
+}
