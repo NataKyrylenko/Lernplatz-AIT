836345390642f3b0b6088cf4e54e16ecda542a67
NataKyrylenko <natalik10107919@gmail.com>
2023-11-15 07:33:33 +0100

    Task1

--- src/HomeWorks/HomeWork6.java
+++ src/HomeWorks/HomeWork6.java
@@ -3,13 +3,13 @@ package HomeWorks;
 public class HomeWork6 {
     public static void main(String[] args) {
        // Task1
-        // int a = 4, b = 3;
-        // if (a!=b) {
-        //     System.out.println(a+b);
-        //  }
-        // else if(a==b) {
-        //     System.out.println((a+b)*2);
-        // }
+        int a = 4, b = 3;
+        if (a!=b) {
+            System.out.println(a+b);
+         }
+        else if(a==b) {
+            System.out.println((a+b)*2);
+        }
 
         // Task2 
         int c = 1, d = 6, e = 6;
