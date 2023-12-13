8592a1177559943c1d8121a472bd6383a25bc974
NataKyrylenko <natalik10107919@gmail.com>
2023-12-13 09:03:06 +0100

    added subclasss labrador

--- src/HomeWorks/HomeWork24/HomePets.java
+++ src/HomeWorks/HomeWork24/HomePets.java
@@ -1,38 +1,67 @@
 package HomeWorks.HomeWork24;
 
 public class HomePets {
+    private String food;
+    private double amount;
+    private String name;
+
+    public HomePets(String food, double amount, String name){
+        this.food = food;
+        this.amount = amount;
+        this.name = name;
+    }
 
     public void needsFood(){
-        System.out.println("Pets needs food");
+        System.out.println(name + " needs " + food + " in the " + amount + " kg");
     }
 }
-
-
 class Dog extends HomePets {
-    public void needsFood(){
-        System.out.println("Dog need Pedigree");
-    }
     
+    public Dog( String food, double amount, String name){
+        super(food, amount, name);
+        
+    }
+    public void needsFood( String food, double amount, String name){
+        System.out.println(name + " need " + food + amount + " kg");
+    }   
+}
+class Labrador extends Dog{
+    private String toys;
+    
+    public Labrador(String  food, double amount,String name, String toys){
+        super(food, amount, name);
+        this.toys = toys;
+    }
+    public void needsFood(String food, double amount, String name){
+        System.out.println( name + " need " + food + amount + " kg");
+    } 
+    public void needsToys(String toys, String name){
+        System.out.println(name + " is playing with a " + toys);
+    }
+
 }
 
-class Cat extends HomePets {
-    public void needsFood(){
-        System.out.println("Cat need Whiskas");
-    }
-    
-}
 
-class Parrot extends HomePets {
-    public void needsFood(){
-        System.out.println("Parrot need grain");
-    }
-    
-}
 
-class Fish extends HomePets {
-    public void needsFood(){
-        System.out.println("Fish need Vipagran");
-    }
+
+
+// class Cat extends HomePets {
+//     public void needsFood(){
+//         System.out.println("Cat need Whiskas");
+//     }
+// }
+
+// class Parrot extends HomePets {
+//     public void needsFood(){
+//         System.out.println("Parrot need grain");
+//     }
     
-}
+// }
+
+// class Fish extends HomePets {
+//     public void needsFood(){
+//         System.out.println("Fish need Vipagran");
+//     }
+    
+// }
 
--- src/HomeWorks/HomeWork24/Main.java
+++ src/HomeWorks/HomeWork24/Main.java
@@ -2,18 +2,16 @@ package HomeWorks.HomeWork24;
 
 public class Main {
     public static void main(String[] args) {
-        HomePets pets = new HomePets();
-        HomePets dog = new Dog();
-        HomePets cat = new Cat();
-        HomePets parrot = new Parrot();
-        HomePets fish = new Fish();
+        HomePets pets = new HomePets("food", 2.3, "Pets");
+        HomePets dog = new Dog("Pedegree", 1.1, "Dog");
+        Labrador labrador = new Labrador("Pedegree", 1.1, "Labrador", "ball");
+        
 
 
         pets.needsFood();
         dog.needsFood();
-        cat.needsFood();
-        parrot.needsFood();
-        fish.needsFood();
+        labrador.needsFood();
+        labrador.needsToys("ball","Labrador");
 
     }
     
