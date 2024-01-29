51b63e8edae572f2e8f550ed6229ae93f45c2350
NataKyrylenko <natalik10107919@gmail.com>
2024-01-29 10:19:09 +0100

    add task3

--- src/HomeWorks/HomeWork47/Main.java
+++ src/HomeWorks/HomeWork47/Main.java
@@ -2,25 +2,32 @@ package HomeWorks.HomeWork47;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> persons = List.of(
-                new Person("John", 24),
+                new Person("John", 34),
                 new Person("Ann", 21),
                 new Person("Bill", 16),
-                new Person("Jack", 20),
-                new Person("Bob", 31),
+                new Person("Jack", 60),
+                new Person("Bob", 51),
                 new Person("Kate", 17),
-                new Person("Alina", 22)
-        );
+                new Person("Alina", 22),
+                new Person("Sven", 55)
 
-        Map<Integer, List<Person>> groupedAge = groupAge(persons);
-        groupedAge.forEach((age, person) -> 
-                    System.out.println("Age " + age + ": " + person));
-        System.out.println("Total age above 18: " + summeAge(persons));
+        );
+        groupAge(persons).forEach((key,value) -> System.out.println(key + " -> " + value.stream().map(Person::getName).collect(Collectors.toList())));
+
+        System.out.println("Total age above 18 is: " + summeAge(persons));
         System.out.println(returnName(persons));
+
+        //Task3
+        System.out.println(countWords("aaaaaa cccc aab bbbb a ff aaa" , "a"));
+        System.out.println(countWords("aaaaaa cccc aab bbbb a ff aaa" , "aaa"));
+        System.out.println(countWords("aaaaaa cccc aab bbbb a ff aaa" , "aa"));
     }
 
 
@@ -31,8 +38,7 @@ public class Main {
     public static int summeAge(List<Person> persons){
         return persons.stream()
                 .filter(person -> person.getAge() > 18)
-                .mapToInt(Person::getAge)
-                .sum();
+                .mapToInt(Person::getAge).sum();
     }
 
     public static String returnName(List<Person> persons){
@@ -40,5 +46,12 @@ public class Main {
                 .filter(person -> person.getAge() > 18) 
                 .map(Person::getName)
                 .collect(Collectors.joining(" and " , "At the moment ", " are of legal age "));
+    }
+
+    //Task3
+    public static long countWords(String sentence, String pattern){
+        return Stream.of(sentence.split(" "))
+                    .filter(w -> w.startsWith(pattern))
+                    .count();
     }
 }
